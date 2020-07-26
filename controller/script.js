8c2ae5b6f08e04b70bb9f13617ca3492d85c6336
Török Attila <torcsiattila93@gmail.com>
2020-07-26 21:33:29 +0200

    feat(main): hide the logout button on phone if the scroll reached the end of the page

--- controller/script.js
+++ controller/script.js
@@ -1,8 +1,18 @@
-$(document).ready(function() {
+$(document).ready(function () {
     var colors = ['#fff740', '#ef820d', '#fda50f', '#cf9812a'];
 
-    $('.note').each(function() {
+    $('.note').each(function () {
         var newColor = colors[Math.floor(Math.random() * colors.length)];
         $(this).css('background-color', newColor)
     });
 });
+
+window.onscroll = () => {
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+        if (((window.innerHeight + window.scrollY) >= document.body.offsetHeight)) {
+            $('.logout-button').fadeOut();
+        } else {
+            $('.logout-button').fadeIn();
+        }
+    }
+}
