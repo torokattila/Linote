3b012a4f6445ca0d1469e38560ca22d7b705f606
Török Attila <torcsiattila93@gmail.com>
2020-08-24 23:00:36 +0200

    refactor(auth): modified auth file

--- controller/auth.js
+++ controller/auth.js
@@ -4,15 +4,15 @@ require('dotenv').config();
 
 module.exports = {
     'facebookAuth' : {
-        'clientID': process.env.FACEBOOK_APP_ID,
-        'clientSecret': process.env.FACEBOOK_SECRET,
+        'clientID': '352936062401441',
+        'clientSecret': 'fd64c28011b7222295f97835b4d29b6b',
         'callbackURL': 'http://localhost:3000/auth/facebook/callback',
         'profileFields': ['id', 'emails', 'name']
     },
 
     'googleAuth' : {
-        'clientID': process.env.GOOGLE_APP_ID,
-        'clientSecret': process.env.GOOGLE_SECRET,
+        'clientID': '441246304601-2ri6qhkg6gkfidvklq8qavkehpeis665.apps.googleusercontent.com',
+        'clientSecret': '_8kEosXq3tyLwjRCm7VV83vk',
         'callbackURL': 'http://localhost:3000/auth/google/callback',
         'profileFields': ['id', 'emails', 'name']
     }
