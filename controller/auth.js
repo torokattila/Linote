be6af42409e3a0f1f682b238f5f6750c44081427
Török Attila <torcsiattila93@gmail.com>
2020-08-26 22:59:55 +0200

    refactor(auth): modified social auth links

--- controller/auth.js
+++ controller/auth.js
@@ -6,14 +6,14 @@ module.exports = {
     'facebookAuth' : {
         'clientID': '352936062401441',
         'clientSecret': 'fd64c28011b7222295f97835b4d29b6b',
-        'callbackURL': 'linote.herokuapp.com/auth/facebook/callback',
+        'callbackURL': '/auth/facebook/callback',
         'profileFields': ['id', 'emails', 'name']
     },
 
     'googleAuth' : {
         'clientID': '441246304601-2ri6qhkg6gkfidvklq8qavkehpeis665.apps.googleusercontent.com',
         'clientSecret': '_8kEosXq3tyLwjRCm7VV83vk',
-        'callbackURL': 'linote.herokuapp.com/auth/google/callback',
+        'callbackURL': '/auth/google/callback',
         'profileFields': ['id', 'emails', 'name']
     }
 }
