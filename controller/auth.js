0fdfafec71005f8e9c0b53620a0071046a2cbf9d
Török Attila <torcsiattila93@gmail.com>
2020-08-30 18:43:15 +0200

    refactor(auth): hide the social secrets

--- controller/auth.js
+++ controller/auth.js
@@ -4,15 +4,15 @@ require('dotenv').config();
 
 module.exports = {
     'facebookAuth' : {
-        'clientID': '352936062401441',
-        'clientSecret': 'fd64c28011b7222295f97835b4d29b6b',
+        'clientID': process.env.FACEBOOK_APP_ID,
+        'clientSecret': process.env.FACEBOOK_SECRET,
         'callbackURL': '/auth/facebook/callback',
         'profileFields': ['id', 'emails', 'name']
     },
 
     'googleAuth' : {
-        'clientID': '441246304601-2ri6qhkg6gkfidvklq8qavkehpeis665.apps.googleusercontent.com',
-        'clientSecret': '_8kEosXq3tyLwjRCm7VV83vk',
+        'clientID': process.env.GOOGLE_APP_ID,
+        'clientSecret': process.env.GOOGLE_SECRET,
         'callbackURL': '/auth/google/callback',
         'profileFields': ['id', 'emails', 'name']
     }
